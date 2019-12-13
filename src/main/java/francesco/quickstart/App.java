a65adf8d01b301d137be07be9d0cc203b51520e0
Francesco Frigati <f.frigati@campus.unimib.it>
2019-12-13 21:58:55 +0100

    Primo commit

---
+++ src/main/java/francesco/quickstart/App.java
@@ -0,0 +1,12 @@
+package francesco.quickstart;
+
+import com.google.gson.Gson;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Gson gson = new Gson();
+        System.out.println(gson.toJson("Hello World!") );
+    }
+}
